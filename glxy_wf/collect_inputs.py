cccd73e264740f2feaff05c3ffc5bc60cf15d32a
suecra39@gmail.com <perssond@metal-1-0.local>
2021-03-29 10:20:17 -0700

    Code base ready for mcmcicro_v0.3.0

--- glxy_wf/collect_inputs.py
+++ glxy_wf/collect_inputs.py
@@ -214,8 +214,10 @@ def upload_dataset(gi, data_path, file_type, folder_name, link="link_to_files"):
         logging.info("File type: %s", file_type)
         logging.info("File path: %s", data_path)
         if os.path.isabs(data_path):
-            galaxy_path = gi.libraries.upload_from_galaxy_filesystem(
-                lib_id, data_path, folder_id, link_data_only=link, file_type=file_type)[0]
+            galaxy_path = gi.libraries.upload_file_from_local_path(                  
+                lib_id, data_path, folder_id, file_type=file_type)[0] 
+            #galaxy_path = gi.libraries.upload_from_galaxy_filesystem(
+            #    lib_id, data_path, folder_id, link_data_only=link, file_type=file_type)[0]
         else:
             galaxy_path=gi.libraries.upload_file_from_server(
                 lib_id, data_path, folder_id, link_data_only=link, file_type=file_type)[0]
@@ -250,3 +252,5 @@ def create_dataset_or_collection_in_history(gi, name, history_id, ids):
 
     return collection
 
+if __name__ == "__main__":
+    collect_inputs(sys.argv[1])
